4483f32597acdeae4c1afbd9a47bb49ccc677170
GaudiNicki <niklas.witzel@nordakademie.de>
2021-06-13 23:08:03 +0200

    use parameter to specify style sheet

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,25 +1,43 @@
 const index = (req, res) => {
-    res.render('index');
+    res.render('index', { 
+        title: 'Home',
+        styleFile: 'indexStyles.css'
+    });
 };
 
 const me = (req, res) => {
-    res.render('me', { title: 'Me' });
+    res.render('me', { 
+        title: 'Me',
+        styleFile: 'meStyles.css' 
+    });
 };
 
 const coding = (req, res) => {
-    res.render('coding', { title: 'Coding' });
+    res.render('coding', { 
+        title: 'Coding',
+        styleFile: 'codingStyles.css' 
+    });
 };
 
 const youtube = (req, res) => {
-    res.render('youtube', { title: 'YouTube' });
+    res.render('youtube', { 
+        title: 'YouTube',
+        styleFile: 'youtubeStyles.css' 
+    });
 };
 
 const imprint = (req, res) => {
-    res.render('imprint', { title: 'Impressum' });
+    res.render('imprint', { 
+        title: 'Impressum',
+        styleFile: 'imprintStyles.css' 
+    });
 };
 
 const privacy = (req, res) => {
-    res.render('privacy', { title: 'Datenschutz' });
+    res.render('privacy', { 
+        title: 'Datenschutz',
+        styleFile: 'privacyStyles.css' 
+    });
 };
 
 module.exports = {
