cbd49a2c19a0d85136108696eadd4d477438d88a
Niklas Witzel <witzel.niklas@gmx.de>
2022-09-01 21:45:29 +0200

    created local_app.js

---
+++ local_app.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const routes = require('./routes/routes');
+
+// ports
+const httpPort = 8080;
+const httpsPort = 8443;
+
+// create http server
+const httpApp = express();
+const httpServer = http.createServer(httpApp);
+
+httpServer.listen(httpPort, () => console.log('listening for http requests on port', httpPort));
+
+// redirect all http requests to https
+//httpApp.get('*', (req, res) => res.redirect('https:' + req.headers.host + req.url));
+
+// create https credentials
+//const credentials = {
+//    key: fs.readFileSync('/etc/letsencrypt/live/gaudinicki.de/privkey.pem'),
+//    cert: fs.readFileSync('/etc/letsencrypt/live/gaudinicki.de/fullchain.pem')
+//}
+
+// create httpsServer
+//const app = express();
+//const httpsServer = https.createServer(credentials, app);
+
+//httpsServer.listen(httpsPort, () => console.log('listening for https requests on port', httpsPort));
+
+// enable reverse proxy support
+httpApp.enable('trust proxy');
+
+// register view engine
+httpApp.set('view engine', 'ejs');
+
+// middlewares
+httpApp.use(express.static('public'));
+httpApp.use(express.urlencoded({ extended: true }));
+
+// routes
+httpApp.use('/', routes);
+
+// 404 page
+httpApp.use((req, res) => res.status(404).render('404', { title: '404' }));
